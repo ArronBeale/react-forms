8a77db2a13f371712ba7fefdaaefc93062157b2e
Arron <beale86@msn.com>
2023-07-15 13:15:21 +0000

    Add useRef and useState form validation to show each type

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,11 +1,38 @@
+import { useRef, useState } from 'react'; // Use useRef or useState, not both. ref for submission, state for active validation
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, SetEnteredName] = useState('');
+
+  const nameInputChangeHandler = (event) => {
+    SetEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+
+    // nameInputRef.current.value = ''; NOT IDEAL, DON'T MANIPULATE THE DOM if using ref
+
+    SetEnteredName('');
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          ref={nameInputRef}
+          type='text'
+          id='name'
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
-      <div className="form-actions">
+      <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
